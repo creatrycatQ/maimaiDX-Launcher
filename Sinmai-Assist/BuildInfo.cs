0a3013b3a152870b14b6f2e5875852adabe452b5
creatrycatQ <creatrycat@gmail.com>
2026-06-12 22:16:44 +0800

    精简Sinmai-Assist：仅保留配置编辑器相关文件

--- Sinmai-Assist/BuildInfo.cs
+++
@@ -1,6 +0,0 @@
-namespace SinmaiAssist { 
-    public static partial class BuildInfo { 
-        public const string CommitHash = "NOT SET"; 
-        public const string BuildDate = "2026-06-10T21:45:18.5076228+08:00"; 
-    } 
-} 
